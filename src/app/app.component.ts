604e0fd690caab5cadaa35b7b38e389bd9ab1e8f
Sampson Orson Jackson <sampson.orson@gmail.com>
2023-06-04 00:11:01 +0100

    chore: change prefix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   standalone: true,
-  selector: 'app-root',
+  selector: 'so-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
