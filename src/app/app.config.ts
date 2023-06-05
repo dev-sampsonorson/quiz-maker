b7c8e62d366e860a8e7ec2aac29b579aec136753
Sampson Orson Jackson <sampson.orson@gmail.com>
2023-06-05 08:26:40 +0100

    chore: add angular material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,12 @@ import { provideHttpClient } from "@angular/common/http";
 import { ApplicationConfig } from "@angular/core";
 import { provideRouter } from "@angular/router";
 import { appRoutes } from "./app.routes";
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes),
     provideHttpClient(),
-  ]
+    provideAnimations()
+]
 };
